28696aec2c94e5b7eb9566dbe0a91b109a2d81a6
axesgeeks2021 <kamalsingh87912711@gmail.com>
2023-08-29 12:29:45 +0530

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01727a722f9f392f"; }
+/******/ 		__webpack_require__.h = function() { return "38b5743fa3d0a890"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
