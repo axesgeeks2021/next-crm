eb6a26388b42a2c4f4cc75f49577c22ea2bbb17d
axesgeeks2021 <kamalsingh87912711@gmail.com>
2023-08-30 12:35:54 +0530

    changes

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "10b67556b4e83248"; }
+/******/ 	__webpack_require__.h = function() { return "7d730093f4f90668"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Hp\Desktop\cmsstartpup\src\app\layout.js
+// File: C:\Users\Dell\Desktop\next-crm\src\app\layout.js
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
